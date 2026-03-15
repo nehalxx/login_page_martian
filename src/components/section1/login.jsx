321470ec121046a68057607506b82a52132cfa1b
shreyahjoshi5-sudo <shreyahjoshi5@gmail.com>
2026-03-15 06:33:05 -0700

    Added login component

--- src/components/section1/login.jsx
+++ src/components/section1/login.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+
+
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
